24abed3a153b45e69b7f925bb3535eae6547709d
Joey Kingston <48415221+gkingston0711@users.noreply.github.com>
2021-11-16 10:42:56 -0800

    daily image

--- mars.js
+++ mars.js
@@ -1,10 +1,10 @@
 /*let searchButton = document.querySelector("#search")*/
 
 sendAPIRequest()
+sendImageRequest()
 
 async function sendAPIRequest(){
     let API_KEY= "8w2DmOrxvNdghKOLw0yFdBW0JiXptiUTXd1OZkcS"
-    /*let response = await fetch('https://api.nasa.gov/planetary/apod?api_key=8w2DmOrxvNdghKOLw0yFdBW0JiXptiUTXd1OZkcS');*/
     let response = await fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=8w2DmOrxvNdghKOLw0yFdBW0JiXptiUTXd1OZkcS');
     console.log(response)
     let data =await response.json()
@@ -13,7 +13,18 @@ async function sendAPIRequest(){
 }
 
 function usaApiData(data){
-/*document.querySelector("#content").innerHTML+=data.date*/
-/*document.querySelector("#content").innerHTML+=`<img src="${data.url}">`*/
 document.querySelector("#content").innerHTML+=`<img src="${data.photos[0].img_src}">`
 }
+
+async function sendImageRequest(){
+    let response = await fetch('https://api.nasa.gov/planetary/apod?api_key=8w2DmOrxvNdghKOLw0yFdBW0JiXptiUTXd1OZkcS');
+    console.log(response)
+    let data =await response.json()
+    console.log(data)
+    useImageData(data)
+}
+
+function useImageData(data){
+document.querySelector("#imagecontent").innerHTML+=`<img src="${data.url}">`
+}
+
